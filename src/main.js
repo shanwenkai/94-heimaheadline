1505dab37e85f92b5340f197363739fc945bd2c9
shanwenkai666 <1104186784@qq.com>
2020-02-29 18:55:50 +0800

    login

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.less'
 
 Vue.config.productionTip = false
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="login">
+    <!-- 表单 -->
+    <el-card class="login-card">
+      <!-- 表单内容 -->
+      <div class="title">
+        <img src="../../assets/img/logo_index.png" alt />
+      </div>
+      <!-- 表单 -->
+      <el-form style="margin:20px">
+        <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <!-- 表单域 -->
+        <el-form-item>
+          <el-input style="width:200px" placeholder="请输入验证码"></el-input>
+          <!-- 放置一个按钮 -->
+          <el-button style="float:right" plain>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <!-- 是否同意点击 -->
+          <el-checkbox>我已阅读同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width:100%" type="primary">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang='less' scoped>
+.login {
+  background-image: url(../../assets/img/dee204e23d9a4ec5b173b3287d747596.jpg);
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card {
+    width: 440px;
+    height: 340px;
+    .title {
+      text-align: center;
+      img {
+        width: 200px;
+      }
+    }
+  }
+}
+</style>
