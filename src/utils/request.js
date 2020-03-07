26900055d9a554580cf0c5f5c966af02cbada247
shanwenkai666 <1104186784@qq.com>
2020-03-07 21:46:43 +0800

    遮罩层

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,13 @@
 
 import axios from 'axios'
 import router from '@/router'
+import JSONBig from 'json-bigint'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+
+axios.defaults.transformResponse = [(data) => {
+  return data ? JSONBig.parse(data) : {}
+}]
+
 // 请求拦截器
 axios.interceptors.request.use(function (config) {
   const token = window.localStorage.getItem('user-token')
--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,47 +1,74 @@
 <template>
- <el-card>
+  <el-card v-loading="loading">
     <bread-crumb slot="header">
-       <template slot="title">
-         评论列表
-         </template>
+      <template slot="title">评论列表</template>
     </bread-crumb>
     <el-table :data="list">
-       <!-- 使用el-table-column作为列 -->
-       <!-- prop 表示显示的字段 label表示表头 -->
-       <el-table-column prop="title" width="600"  label="标题"></el-table-column>
-       <el-table-column :formatter="formatterBool" prop="comment_status" label="评论状态"></el-table-column>
-       <el-table-column  prop="total_comment_count" label="总评论数"></el-table-column>
-       <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
-       <el-table-column  label="操作">
-          <!-- 可以放组件 -->
-          <template slot-scope="obj">
-              <el-button  size="small" type='text'>修改</el-button>
-              <el-button  @click="openOrClose(obj.row)" size="small" type='text'>{{obj.row.comment_status ? "关闭" : "打开"}}</el-button>
-          </template>
+      <!-- 使用el-table-column作为列 -->
+      <!-- prop 表示显示的字段 label表示表头 -->
+      <el-table-column prop="title" width="600" label="标题"></el-table-column>
+      <el-table-column :formatter="formatterBool" prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+      <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+      <el-table-column label="操作">
+        <!-- 可以放组件 -->
+        <template slot-scope="obj">
+          <el-button size="small" type="text">修改</el-button>
+          <el-button
+            @click="openOrClose(obj.row)"
+            size="small"
+            type="text"
+          >{{obj.row.comment_status ? "关闭" : "打开"}}</el-button>
+        </template>
 
-       </el-table-column>
+      </el-table-column>
     </el-table>
- </el-card>
+    <el-row type='flex' justify="center" align="middle" style="height:80px">
+      <!-- 分页组件 total 总页码  每页多少条-->
+      <el-pagination background layout="prev, pager, next"
+       :current-page="page.currentPage"
+       :total="page.total"
+       :page-size="page.pageSize"
+       @current-change="changePage"
+       ></el-pagination>
+    </el-row>
+  </el-card>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      list: [{ id: 1, name: '望京', date: '2020' }]
+      page: {
+        total: 0,
+        currentPage: 10,
+        pageSize: 10
+      },
+      list: [
+
+      ],
+      loading: false // 控制laoding遮罩层的显示
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.currentPage = newPage
+
+      this.getComment()
+    },
     getComment () {
+      this.loading = true // 打开遮罩层
       this.$axios({
         url: '/articles',
-        method: 'put',
         params: {
-          response_type: 'comment'
+          response_type: 'comment',
+          page: this.page.currentPage,
+          per_page: this.page.pageSize
         }
-
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
+        this.loading = false // 关闭遮罩层
       })
     },
     formatterBool (row, column, cellValue, index) {
@@ -49,21 +76,24 @@ export default {
     },
     openOrClose (row) {
       const mess = row.comment_status ? '关闭' : '打开 '
-      this.$confirm(`是否确定${mess}`).then(() => {
+      this.$confirm(`是否确定${mess}评论`, '提示').then(() => {
         this.$axios({
           url: '/comments/status',
+          method: 'put',
           params: {
-            article_id: row.id
+            article_id: row.id.toString()
           },
           data: {
             allow_comment: !row.comment_status
           }
-        }).then(() => {
-          this.$message.succes(`${mess}评论成功`)
-          this.getComment()
-        }).catch(() => {
-          this.$message.error(`${mess}评论失败`)
         })
+          .then(() => {
+            this.$message.succes(`${mess}评论成功`)
+            this.getComment()
+          })
+          .catch(() => {
+            this.$message.error(`${mess}评论失败`)
+          })
       })
     }
   },
@@ -74,5 +104,4 @@ export default {
 </script>
 
 <style>
-
 </style>
